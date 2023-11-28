27241dd328168bc5145c8a902701bfda342bcd8c
Kochetov <kochetov.yevhen.dev@gmail.com>
2023-11-28 19:37:36 +0200

    create header and image sections og the post component

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -1,4 +1,6 @@
 import { PostData } from "../_types";
+import { EllipsisHorizontalIcon } from '@heroicons/react/24/solid'
+
 
 interface Props {
   post: PostData;
@@ -6,8 +8,13 @@ interface Props {
 
 const Post = ({ post }: Props) => {
   return (
-    <div>
-      <h1>{post.username}</h1>
+    <div className="bg-white my-7 border rounded-md">
+      <div className="flex items-center p-5">
+        <img className="h-12 rounded-full object-cover border p-1 mr-3" src={post.userImg} alt={post.username} />
+        <p className="font-bold flex-1">{post.username}</p>
+        <EllipsisHorizontalIcon className="h-5" />
+      </div>
+      <img className="objecr-cover w-full" src={post.img} alt="" />
     </div>
   );
 };

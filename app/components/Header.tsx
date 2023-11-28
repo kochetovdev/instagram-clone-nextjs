0f358ddd9139388c27b3b517703ee1c9d124e37c
Kochetov <kochetov.yevhen.dev@gmail.com>
2023-11-28 14:22:08 +0200

    add search bar to the header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 const Header = () => {
   return (
@@ -17,6 +18,16 @@ const Header = () => {
           layout="fill"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
           alt="Instagram logo"
+        />
+      </div>
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <MagnifyingGlassIcon className="h-5 text-gray-500" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
         />
       </div>
       <h1>Right sides</h1>
